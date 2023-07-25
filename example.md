fc4d1e64ff5c8b6fd5647c89e497749dfc17873d
juanppalacios <palaciosvilchis.jpa@gmail.com>
2023-07-24 23:07:44 -0400

    added very simple example

---
+++ example.md
@@ -0,0 +1,8 @@
+
+# Title
+some text here
+## Sub-Title
+some text there
+
+### Sub-Sub-Title
+some text everywhere
--- scripts/markdown_parser.py
+++ scripts/markdown_parser.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
 
+# todo: read about parser tools: https://supunsetunga.medium.com/writing-a-parser-getting-started-44ba70bb6cc9
+
+
+# todo: create a rough but simple parser
+def parser():
+    print('running our parser...')
+    
+    # open up our markdown file
+    markdown_file = []
+    
+    with open('example.md', 'r') as file:
+        markdown_file = file.readlines()
+        
+    for line in markdown_file:
+        print(line)
+        
+    # convert our lines to Latex syntax
+    latex_file = []
+    added_lines = []
+    with open('latex/example.tex', 'r') as file:
+        latex_file = file.readlines()
+        
+    for line in markdown_file:
+        # todo: use the count() method to then choose how many 'sub' keywords to add
+        if '#' in line:
+            added_lines.append('\section*')
+        
+    
+
 if __name__ == '__main__':
-    print('hello world')
+    parser()
 else:
     pass
